ce0af815131655b0d537ca1653ae51274284b83a
Vedant Modi <vedantmodi@gmail.com>
2023-07-27 15:15:54 -0500

    Finished cpp build

---
+++ README.md
@@ -0,0 +1,12 @@
+# ppi
+A simple calculator to find the pixel density (pixels per inch, PPI) in a display given the pixel dimensions and diagonal of the display. I use this when shopping for monitors to do some quick calculations.
+
+## Usage
+### Installation
+To install, download the project files, and copy the binary to your local bin file
+```sudo cp ./ppi /usr/local/bin```
+
+### Running
+Run this command with the dimensions in pixels, and the diagonal of the display in inches.
+
+```ppi width-px height-px diagonal-in```
--- main.cpp
+++ main.cpp
@@ -1,7 +1,36 @@
 #include <iostream>
+#include <string>
+#include <cmath>
 
 using namespace std;
 
+double ppi(double pixel_width, double pixel_height, double inches_diagonal);
+double pixels_diagonal(double width, double height);
+
+const string USAGE = "Usage: ppi width-px height-px diagonal-in\n";
+
 int main (int argc, char *argv[]) {
-    
+    if (argc != 4) {
+        cerr << USAGE;
+        exit(EXIT_FAILURE);
+    }
+
+    const double pixel_width = atof(argv[1]);
+    const double pixel_height = atof(argv[2]);
+    const double inches_diagonal = atof(argv[3]);
+
+    // result is pixels per inch
+    cout << ppi(pixel_width, pixel_height, inches_diagonal);
+
+    return 0;
+}
+
+double ppi(double pixel_width, double pixel_height, double inches_diagonal) {
+    // length of diagonal in pixels / length of diagonal in inches
+    return pixels_diagonal(pixel_width, pixel_height) / inches_diagonal;
+}
+
+double pixels_diagonal(double width, double height) {
+    // Calculate the length of the diagonal in pixels
+    return sqrt((width * width) + (height * height));
 }
